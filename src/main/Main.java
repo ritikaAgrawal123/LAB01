6c8d3a397fc4928d85efd35f28025f9cb9ec5ff1
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2020-04-20 05:40:47 -0700

    class Main:

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,41 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arr = {1, 2, 3, 4, 1, 2, 7};
+        findElement(arr, 2);
+        duplicates(arr);
+    }
+
+    public static void findElement(int[] arr, int ele) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ele) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void duplicates(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        boolean dup = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        System.out.println("duplicate values are:");
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
 
     }
 }
